9abb855b9023ecb3ea7dbdd70d007809d0a7242b
Jill Cates <cates.jill@gmail.com>
2017-11-16 15:07:23 -0800

    Update code to make project name a variable

--- script.sh
+++ script.sh
@@ -3,10 +3,43 @@
 # no file overwritting allowed
 set -o noclobber
 
+# set up 
+echo "Setting up project directory. Project name: $1"
+
+[[ -z "$1" ]] && { echo "Project name is empty." ; exit 1; }
+
 # set up folder structure
 cd ..
-mkdir project
+mkdir $1
 
-cd project
+cd $1
 mkdir doc data results src bin
 
+echo "Folders created."
+
+touch LICENSE.md
+touch CITATION.md
+touch CONDUCT.md
+touch CONTRIBUTING.md
+
+echo "Readme, license, citation, conduct, contributing markdown files created"
+
+touch Makefile
+
+cat > README.md << EOF
+## Project Title
+
+## Data Structure
+
+    |- doc/           # documentation
+    |
+    |- data/          # directory for storing data sets
+    |
+    |- results/       # output for tracking computational
+    |
+    |- src/           # source code
+    |
+    |- bin/           # any compiled binaries or scripts
+EOF
+
+echo "Project successfully created."
